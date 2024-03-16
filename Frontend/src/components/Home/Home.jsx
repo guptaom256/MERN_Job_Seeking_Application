5420e73f7b12d03f25d8fa6e7456a715b80bb065
Om Gupta <guptaom256@gmail.com>
2024-03-16 13:14:25 +0530

    second commit

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -7,15 +7,12 @@ import HowItWorks from "./HowItWorks";
 import PopularCategories from "./PopularCategories";
 import PopularCompanies from "./PopularCompanies";
 
-
-
-
 const Home = () => {
 
-  const { isAuthorized } = useContext(Context);
-    if(!isAuthorized) {
-      return <Navigate to={"/login"}/>
-    }
+  // const { isAuthorized } = useContext(Context);
+  //   if(!isAuthorized) {
+  //     return <Navigate to={"/login"}/>
+  //   }
 
   return (
     <section className='homePage page'>
